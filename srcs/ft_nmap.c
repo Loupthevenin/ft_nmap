d657ebc167d735ea9ae4db067847b2ca127c4fe6
Dang-Minh Tran <dangtran@student.42.fr>
2025-08-18 18:25:48 +0200

    parser.c file created

--- srcs/ft_nmap.c
+++ srcs/ft_nmap.c
@@ -1,12 +1,6 @@
 #include "../includes/ft_nmap.h"
 
-int	parse_args(t_config *config, int argc, char **argv)
-{
-	(void)argc;
-	(void)argv;
-	(void)config;
-	return (0);
-}
+
 
 int	main(int argc, char **argv)
 {
---
+++ srcs/parser.c
@@ -0,0 +1,9 @@
+#include "../includes/ft_nmap.h"
+
+int	parse_args(t_config *config, int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	(void)config;
+	return (0);
+}
