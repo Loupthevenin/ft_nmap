75de0c28181ea2e149e4a7a7a976ed016f18b876
Loupthevenin <loup.thevenin.1@gmail.com>
2025-08-19 11:50:03 +0200

    fix format help screen

--- srcs/utils.c
+++ srcs/utils.c
@@ -2,34 +2,36 @@
 
 void	print_help(void)
 {
+	printf("Help Screen\n");
 	printf("ft_nmap [OPTIONS]\n");
-	printf("--help\t\tPrint this help screen\n");
-	printf("--ports\t\tPorts to scan (eg: 1-10 or 1,2,3 or 1,5-15)\n");
-	printf("--ip\t\tIP address to scan in dot format\n");
-	printf("--file\t\tFile name containing IP addresses to scan\n");
-	printf("--speedup\t[250 max] Number of parallel threads to use\n");
-	printf("--scan\t\tSYN/NULL/FIN/XMAS/ACK/UDP\n");
+	printf("--help Print this help screen\n");
+	printf("--ports ports to scan (eg: 1-10 or 1,2,3 or 1,5-15)\n");
+	printf("--ip ip addresses to scan in dot format\n");
+	printf("--file File name containing IP addresses to scan,\n");
+	printf("--speedup [250 max] number of parallel threads to use\n");
+	printf("--scan SYN/NULL/FIN/XMAS/ACK/UDP\n");
 }
 
-void print_config(const t_config *config)
+void	print_config(const t_config *config)
 {
-    printf("Configuration:\n");
-    printf("  IP Address: %s\n", config->ip ? config->ip : "None");
-    printf("  Ports: %s\n", config->ports);
-    printf("  Speedup: %d\n", config->speedup);
-    printf("  Scan Type: %s\n", config->scan_type ? config->scan_type : "None");
+	printf("Configuration:\n");
+	printf("  IP Address: %s\n", config->ip ? config->ip : "None");
+	printf("  Ports: %s\n", config->ports);
+	printf("  Speedup: %d\n", config->speedup);
+	printf("  Scan Type: %s\n", config->scan_type ? config->scan_type : "None");
 }
 
-
-void free_args(char **args)
+void	free_args(char **args)
 {
-    int i = 0;
-    if (!args)
-        return;
-    
-    while (args[i]) {
-        free(args[i]);
-        i++;
-    }
-    free(args);
+	int	i;
+
+	i = 0;
+	if (!args)
+		return ;
+	while (args[i])
+	{
+		free(args[i]);
+		i++;
+	}
+	free(args);
 }
